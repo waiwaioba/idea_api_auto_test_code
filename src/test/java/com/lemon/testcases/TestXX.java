4b736f5559e8e4d2627e3b9c383b5ffa3b2fc6ff
“hhy” <1425301899@qq.com>
2022-01-18 21:31:57 +0800

    添加了TestXX测试用例类

---
+++ src/test/java/com/lemon/testcases/TestXX.java
@@ -0,0 +1,16 @@
+package com.lemon.testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author 歪歪欧巴
+ * @Description TODO
+ * @date 2022/1/18 21:30
+ * @Copyright 湖南省零檬信息技术有限公司. All rights reserved.
+ */
+public class TestXX {
+    @Test
+    public void testXX(){
+
+    }
+}
