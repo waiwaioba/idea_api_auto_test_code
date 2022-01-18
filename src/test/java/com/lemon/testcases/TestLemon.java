b93f0f63b36aee727f101f4e31cc9c2650e706f6
“hhy” <1425301899@qq.com>
2022-01-18 21:37:27 +0800

    新增了TestLemon测试类

---
+++ src/test/java/com/lemon/testcases/TestLemon.java
@@ -0,0 +1,16 @@
+package com.lemon.testcases;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author 歪歪欧巴
+ * @Description TODO
+ * @date 2022/1/18 21:36
+ * @Copyright 湖南省零檬信息技术有限公司. All rights reserved.
+ */
+public class TestLemon {
+    @Test
+    public void testXX(){
+        System.out.println("XXX");
+    }
+}
